85879eb7782eecb990ecc2d64df7402e7369ddf4
codeChef8888 <96719023+codeChef8888@users.noreply.github.com>
2021-12-27 15:21:33 +0545

    springSecurityBasicInitialization

---
+++ securityInitialtization/src/main/java/spring/security/com/securityInitialtization/SecurityInitialtizationApplication.java
@@ -0,0 +1,13 @@
+package spring.security.com.securityInitialtization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityInitialtizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityInitialtizationApplication.class, args);
+	}
+
+}
---
+++ securityInitialtization/src/test/java/spring/security/com/securityInitialtization/SecurityInitialtizationApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.security.com.securityInitialtization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityInitialtizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
